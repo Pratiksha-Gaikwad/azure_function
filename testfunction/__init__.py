acdfb25abe6570d00ae0dc60dd8f067023a49fe6
Pratiksha Gaikwad <72595119+Pratiksha-Gaikwad@users.noreply.github.com>
2021-02-17 15:05:01 +0530

    "New commit"

--- testfunction/__init__.py
+++ testfunction/__init__.py
@@ -25,7 +25,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully .")
     else:
-        return func.HttpResponse(f"The new build created.",
-
-             status_code=200
-        )
+        return func.HttpResponse(f"The new build created for every new commit.",status_code=200)
